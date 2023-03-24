8b4187a609c0a89a343e4a8aa5c8aef505eb3f83
JYLEE0110 <wndyd0110@gmail.com>
2023-03-24 17:58:58 +0900

    UPDATE 2023.03.24

--- MyProject/src/Chapter1/CalculatorPractice.java
+++ MyProject/src/Chapter1/CalculatorPractice.java
@@ -1,36 +1,75 @@
 package Chapter1;
 
-class Calculator{
-	
-	static int add(int num1, int num2) {
-		return num1+num2;
+import java.util.*;
+
+//① 숫자 두 개를 매개변수의 인자로 전달받아 더하기 메소드를 추가합시다.
+//② 숫자 두 개를 매개변수의 인자로 전달받아 빼기 메소드를 추가합시다.
+//③ 숫자 두 개를 매개변수의 인자로 전달받아 곱하기 메소드를 추가합시다.
+//④ 숫자 두 개를 매개변수의 인자로 전달받아 나누기 메소드를 추가합시다.
+//⑤ 실수 반지름 하나를 매개변수의 인자로 전달받아 원의 둘레를 구해 반환하는 메소드를 반환하는 메소드를 정의
+//⑥ 실수 반지름 하나를 매개변수의 인자로 전달받아 원의 넓이를 구해 반환하는 메소드를 반환하는 메소드를 정의
+//원의 둘레 : 2 x π x r , 월의 넓이 : π x r x r
+//⑦ main() 메소드를 정의하고 각각의 메소드를 호출해서 결과를 콘솔에 출력해봅시다.
+//⑧ 콘솔에서 사용자에게 데이터를 받아 메소드를 호출할 때 사용자에게 받은 데이터를 메소드의 매개변수의 인자로 전달하는 코드를 main() 메소드
+//에 추가해봅시다
+
+class Calculator {
+
+	static long addNum(long num1, long num2) {
+		return num1 + num2;
 	}
-	
-	static int sub(int num1, int num2) {
-		return num1-num2;
+
+	static long subNum(long num1, long num2) {
+		return num1 - num2;
 	}
-	
-	static int mul(int num1, int num2) {
-		return num1*num2;
+
+	static long mulNum(long num1, long num2) {
+		return num1 * num2;
 	}
-	
-	static int div(int num1, int num2) {
-		return num1/num2;
+
+	static float divNum(float num1, float num2) {
+		return num1 / num2;
 	}
-	
+
+	static double calCircumference(int r) {
+		return 2 * Math.PI * r;
+	}
+
+	static double calCirArea(int r) {
+		return Math.PI * Math.pow(r, 2);
+	}
 }
 
 public class CalculatorPractice {
 
 	public static void main(String[] args) {
-		
-		int num1 = 5;
-		int num2 = 3;
-		
-		System.out.printf("%d + %d = %d\n",num1,num2, Calculator.add(num1, num2));
-		System.out.printf("%d - %d = %d\n",num1,num2, Calculator.sub(num1, num2));
-		System.out.printf("%d * %d = %d\n",num1,num2, Calculator.mul(num1, num2));
-		System.out.printf("%d / %d = %d\n",num1,num2, Calculator.div(num1, num2));
+
+		long num1;
+		long num2;
+		int r;
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.print("Input num1 : ");
+		num1 = scan.nextLong();
+
+		System.out.print("Input num2 : ");
+		num2 = scan.nextLong();
+
+		System.out.println();
+
+		System.out.printf("%d + %d = %d\n", num1, num2, Calculator.addNum(num1, num2));
+		System.out.printf("%d - %d = %d\n", num1, num2, Calculator.subNum(num1, num2));
+		System.out.printf("%d * %d = %d\n", num1, num2, Calculator.mulNum(num1, num2));
+		System.out.printf("%d / %d = %f\n", num1, num2, Calculator.divNum(num1, num2));
+
+		System.out.println();
+
+		System.out.print("Input radius : ");
+		r = scan.nextInt();
+
+		System.out.printf("반지름이 %d인 원의 둘레는 %.2f\n", num1, Calculator.calCircumference(r));
+		System.out.printf("반지름이 %d인 원의 넒이는 %.2f\n", num1, Calculator.calCirArea(r));
 	}
 
 }
--- MyProject/src/Chapter1/Member.java
+++ MyProject/src/Chapter1/Member.java
@@ -1,22 +1,63 @@
 package Chapter1;
 
-class MemberInfo{
+//① String 타입의 이름을 저장할 수 있는 변수 name을 정의해봅시다.
+//② int 타입의 나이를 저장할 수 있는 변수 age를 정의해봅시다.
+//③ double 타입의 키를 저장할 수 있는 변수 height를 정의해봅시다.
+//④ boolean 타입의 JAVA책의 보유 여부를 저장할 수 있는 변수 hasBook를 정의해봅시다.
+//⑤ 이름과 나이, 키, 책의 보유 여부를 출력해봅시다.
+
+class MemberInfo {
+	static int currentYear = 2023;
+	
+	int birthYear;
 	String name;
 	int age;
 	double height;
 	boolean hasBook;
-	
-	MemberInfo(){}
-	
-	MemberInfo(String name, int age, double height, boolean hasBook){
+
+	MemberInfo() {
+		this.age = currentYear-this.birthYear;
+	}
+
+	MemberInfo(String name,int birthYear, double height, boolean hasBook) {
 		this.name = name;
-		this.age = age;
+		this.birthYear = birthYear;
 		this.height = height;
 		this.hasBook = hasBook;
 	}
 	
+	void calAge() {// 현재연도와 태어난 연도로 age를 구한 뒤 인스턴스 변수에 대입
+		int age = currentYear-birthYear+1; // 지역변수 age
+		this.age = age;
+	}
+	
+	void printCheckFreeVaccine() {
+		
+		if(age<15 || age>=65) {
+			System.out.println(age + "세이므로 15세미만 또는 65세이상입니다. 백신무료접종이 가능합니다.");
+		}else {
+			System.out.println(age + "세이므로 15세이상 65세미만입니다. 백신무료접종 대상이 아닙니다.");
+		}
+	}
+	
+	void printCheckHealth() {
+//		currentYear%2 == birthYear%2
+		if(age>=20 && (currentYear+birthYear)%2==0) {
+			System.out.println("건강검진 무료 대상입니다.");
+		}else {
+			System.out.println("건강검진 무료 대상이아닙니다.");
+		}
+		if(age>=40) {
+			System.out.println("무료 암 검진 대상자입니다.");
+		}else {
+			System.out.println("무료 암 검진 대상자가 아닙니다.");
+			
+		}
+	}
+
 	public void displayInfo() {
 		System.out.println("name : " + name);
+		System.out.println("birth Of year : " + birthYear);
 		System.out.println("age : " + age);
 		System.out.println("height : " + height);
 		System.out.println("hasBook : " + hasBook);
@@ -26,8 +67,20 @@ class MemberInfo{
 public class Member {
 
 	public static void main(String[] args) {
-		MemberInfo student1 = new MemberInfo("JY LEE", 26, 189.9, true);
+		
+		MemberInfo student1 = new MemberInfo("JY LEE", 1998, 189.9, true);
+		student1.calAge(); //현재연도와 태어난 연도로 age를 구한 뒤 인스턴스 변수에 대입하는 메소드
 		student1.displayInfo();
+		student1.printCheckFreeVaccine();
+		student1.printCheckHealth();
+		
+		System.out.println("-----------------------------------------------");
+		
+		MemberInfo student2 = new MemberInfo("KP LEE", 1957, 199.9, false);
+		student2.calAge();
+		student2.displayInfo();
+		student2.printCheckFreeVaccine();
+		student2.printCheckHealth();
 	}
 
 }
