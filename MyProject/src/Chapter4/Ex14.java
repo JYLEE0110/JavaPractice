17252e90018b6fc9f512a99c5aa546675389dd12
JYLEE0110 <wndyd0110@gmail.com>
2023-03-24 09:39:52 +0900

    Update Ex14.java

--- MyProject/src/Chapter4/Ex14.java
+++ MyProject/src/Chapter4/Ex14.java
@@ -4,13 +4,17 @@ public class Ex14 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int numA;
-//		int numB;
-		
+		//		int numA;
+		//		int numB;
+
 		for(int i = 1; i<=9; i++) {
 			for(int j=1; j<=9; j++) {
-				if((i+j)==9) {
-					System.out.printf("%d , %d\n",i,j);
+				int num1=i*10+j;
+				int num2=j*10+i;
+				int sum = num1 + num2;
+				
+				if(num1+num2 == 99) {
+					System.out.printf("%d + %d = %d\n",num1, num2, sum);
 				}
 			}
 		}
