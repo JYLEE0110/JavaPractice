c0eb6620893c7f9f0bb67a0d8e11d24ead401f79
JYLEE0110 <wndyd0110@gmail.com>
2023-03-28 19:20:20 +0900

    Update ContactMain.java

--- MyProject/src/Chapter5/ContactMain.java
+++ MyProject/src/Chapter5/ContactMain.java
@@ -6,7 +6,7 @@ public class ContactMain {
 		
 		Contact student1 = new Contact();
 		
-		student1.inputData();//scanner로 데이터 입력
+		student1.inputData();//scanner로 데이터 입력하는 메소드
 		System.out.println();
 		student1.printData();//전체 데이터 출력 메소드
 		
@@ -20,6 +20,18 @@ public class ContactMain {
 		System.out.println("생일 : " + student1.getBirth());
 		System.out.println("그룹 : " + student1.getGroup());
 		
+		System.out.println();
+		
+		//setter로 데이터 수정
+		student1.setName("이주용");
+		student1.setPhoneNum("010-5420-XXXX");
+		student1.setEmail("wndyd@gmail.com");
+		student1.setAddress("성남시 분당구");
+		student1.setBirth("980110");
+		student1.setGroup("1501호");
+		
+		student1.printData(); //수정된 데이터 출력
+	
 	}
 
 }
