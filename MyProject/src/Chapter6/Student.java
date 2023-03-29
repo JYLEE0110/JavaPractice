d646b53e782b8f1c2768d9b1dd66da6aa0fdd951
JYLEE0110 <wndyd0110@gmail.com>
2023-03-29 16:57:48 +0900

    UPDATE 2023. 3.29

---
+++ MyProject/src/Chapter6/Student.java
@@ -0,0 +1,71 @@
+package Chapter6;
+
+public class Student {
+
+	private String name;
+	private int korScore;
+	private int engScore;
+	private int mathScore;
+
+	public Student(String name, int korScore, int engScore, int mathScore) {
+		super();
+		this.name = name;
+		this.korScore = korScore;
+		this.engScore = engScore;
+		this.mathScore = mathScore;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKorScore() {
+		return korScore;
+	}
+
+	public void setKorScore(int korScore) {
+		this.korScore = korScore;
+	}
+
+	public int getEngScore() {
+		return engScore;
+	}
+
+	public void setEngScore(int engScore) {
+		this.engScore = engScore;
+	}
+
+	public int getMathScore() {
+		return mathScore;
+	}
+
+	public void setMathScore(int mathScore) {
+		this.mathScore = mathScore;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", korScore=" + korScore + ", engScore=" + engScore + ", mathScore="
+				+ mathScore + "]";
+	}
+
+	//총 합을 구하는 메소드
+	public int sumScore() {
+		int totalScore = korScore + engScore + mathScore;
+
+		return totalScore;
+	}
+	//평균을 구하는 메소드
+	public float avgScore() {
+		int totalScore = sumScore();
+		float avg = totalScore / (float) 3;
+
+		return avg;
+	}
+
+}
