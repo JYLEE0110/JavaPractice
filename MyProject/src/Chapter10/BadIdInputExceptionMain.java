8c1fbffc30bf9d7a3b0172044b2d6155d34f598d
JYLEE0110 <wndyd0110@gmail.com>
2023-04-05 10:03:05 +0900

    add Annotation

--- MyProject/src/Chapter10/BadIdInputExceptionMain.java
+++ MyProject/src/Chapter10/BadIdInputExceptionMain.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+//1. 콘솔에서 사용자 아이디를 입력 받아 정상적인 영문자와 숫자로만 이루어진 값을 입력했는지 확인하는 프로그램을 만들어봅시다. 
+//① 사용자 예외 클래스를 정의해서 예외를 발생 시켜 봅시다.
+//② 예외 클래스 이름은 BadIdInputException이라고 정의합시다.
+
 public class BadIdInputExceptionMain {
 
 	public static void main(String[] args) {
